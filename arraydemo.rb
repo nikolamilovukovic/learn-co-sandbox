a5aa391204f4bceae3c319ff0c57c1579e955b64
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-11 19:50:00 +0000

    Automatically backed up by Learn

---
+++ arraydemo.rb
@@ -0,0 +1,10 @@
+school = {
+  instructors: ["Ian", "Johann", "Alex"],
+  students: ["Andrew", "Howard", "Terrance", "Daniel", "Rachel", "Natalie"],
+  classes: ["Software Engineering", "Data Science"]
+}
+
+school[grades:] = "a",  "b",  "c"
+
+puts school
+
